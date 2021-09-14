2c018deffe66fbaf1cc6754260d1424502b151ba
Evgeny <canimore@yandex.ua>
2021-09-14 13:46:58 +0300

    обновил гуи

--- app_gui.py
+++ app_gui.py
@@ -12,10 +12,10 @@ layout = [
       sg.Text("Приложения по защите информации лабараторная работа 1")
     ],
     [
-      sg.Text('Файл 1'), sg.InputText(key="-FOLDER_1-"), sg.FileBrowse(), sg.Button('Зашифровать', key='-ENCRYPT-')
+      sg.Text('Файл 1'), sg.InputText(key="-FOLDER_1-"), sg.FileBrowse(), sg.Button('Зашифровать', key='-ENCRYPT-'), sg.InputText(key="-FOLDER_1_OUT-")
     ],
     [
-      sg.Text('Файл 2'), sg.InputText(key="-FOLDER_2-"), sg.FileBrowse(),sg.Button('Расшифровать', key='-DECRYPT-')
+      sg.Text('Файл 2'), sg.InputText(key="-FOLDER_2-"), sg.FileBrowse(),sg.Button('Расшифровать', key='-DECRYPT-'), sg.InputText(key="-FOLDER_2_OUT-")
     ],
     [
       sg.Text('Ключ цифровой'), sg.InputText(key="-KEY-",default_text='0')
@@ -47,30 +47,46 @@ while True:
     if event == '-ENCRYPT-':
         file = values['-FOLDER_1-']
         key = key_in if key_in else '0'
+        encrypt_path = values['-FOLDER_1_OUT-']
+        encrypt_path = encrypt_path if len(encrypt_path) > 0 else 'encrypt.txt'
+        gr = Gronsfeld(key)
+
         # Если есть путь то шифруем
         if os.path.exists(file):
             file_data = get_file_data(file)
-            gr = Gronsfeld(key)
-            out.Update(gr.encrypt(file_data))
+            encrypted = gr.encrypt(file_data)
+
         # шифровка из поля
         elif len(values['-OUT-']) > 0:
             file_data = values['-OUT-']
-            gr = Gronsfeld(key)
-            out.Update(gr.encrypt(file_data))
+            encrypted = gr.encrypt(file_data)
+
+        else:
+            break
+        write_data(encrypt_path, encrypted)
+        out.Update(encrypted)
+
     if event == '-DECRYPT-':
         file = values['-FOLDER_2-']
         key = key_in if key_in else '0'
+        decrypt_path = values['-FOLDER_2_OUT-']
+        decrypt_path = decrypt_path if len(decrypt_path) else 'decrypt.txtx'
+        gr = Gronsfeld(key)
+
         # Если есть путь то шифруем
         if os.path.exists(file):
             file_data = get_file_data(file)
-            gr = Gronsfeld(key)
-            out.Update(gr.decrypt(file_data))
+            decrypt = gr.decrypt(file_data)
+
         # шифровка из поля
         elif len(values['-OUT-']) > 0:
             file_data = values['-OUT-']
-            gr = Gronsfeld(key)
-            out.Update(gr.decrypt(file_data))
+            decrypt = gr.decrypt(file_data)
+        else:
+            break
 
+        write_data(decrypt_path, decrypt)
+        out.Update(decrypt)
             
 
 window.close()
