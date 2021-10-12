90020429d83f324f2350264267fc12b5e9159a0d
Asgard <canimore@yandex.ua>
2021-10-12 12:16:39 +0300

    Закончил консольный интерфейс

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import argparse
 import random
 import sys
+import time
+import os
 
 from os import listdir
 from gronsfeld import Gronsfeld
@@ -39,50 +41,58 @@ def get_alphabet(file_name):
     return return_string
 
 
-def main(action, text, key, output):
+def main(action, text, key, output, log = False):
     """Выполнить весь скрипт действий."""
     origin = text
     a = Gronsfeld(key)
+    write_data('key.key', key)
+    #print('key',key)
     encrypted = ''
     decrypted = ''
     if(action == 'encrypt'):
         encrypted = a.encrypt(text)
-        write_data('encrypted.txt', encrypted)
+        write_data('encrypted.txt' if output == '' else output, encrypted)
     elif action == 'decrypt':
         decrypted = a.decrypt(text)
-        write_data('decrypted.txt', decrypted)
+        write_data('decrypted.txt' if output == '' else output, decrypted)
     else:
         encrypted = a.encrypt(text)
         decrypted = a.decrypt(encrypted)
-    print('key:       {0}'.format(key))
-    print('aplhabet:  {0}'.format(a.alphabet))
-    print('origin:    {0}'.format(origin))
-    print('encrypted: {0}'.format(encrypted))
-    print('decrypted: {0}'.format(decrypted))
+    if log:
+        print('key:       {0}'.format(key))
+        print('aplhabet:  {0}'.format(a.alphabet))
+        print('origin:    {0}'.format(origin))
+        print('encrypted: {0}'.format(encrypted))
+        print('decrypted: {0}'.format(decrypted))
+
+    print('Выполнение программы закончено')
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-a', '--action', dest='action', help='Тип действия Encrypt or decrypt', required=True)
-    parser.add_argument('-k', '--key', dest='key', help='Ключ для шифрования', default='')
-    parser.add_argument('-i', '--input', dest='input', help='Входящий файл', required=True)
-    parser.add_argument('-o', '--output', dest='output', help='Исходящий файл, если не указан будет создан автоматически out.txt', default='out.txt')
+    parser.add_argument('-k', '--key', dest='key', help='Ключ для шифрования или путь к файлу с ключем.Если ключ не указан создастся случайный с длиной файла', default='')
+    parser.add_argument('-i', '--input', dest='input', help='Путь к файлу', required=True)
+    parser.add_argument('-o', '--output', dest='output', help='Исходящий файл, если не указан будет создан автоматически', default='')
+    parser.add_argument('-l', '--logs', dest='logs', action='store_true', default=False, help='Выводить ли лог')
     args = parser.parse_args()
 
-    print(args)
+    start_time = time.time()
 
     # Тип действия
     action = args.action.lower()
     # Данные файла
     text = get_file_data(args.input)
     # Ключ для шифрования
-    key = args.key if args.key != '' else ''.join([str(random.randint(0, 9)) for i in range(len(text))])
-    print(key)
+
+    key = ''
+    if args.key.isdigit():
+        key = args.key
+    elif os.path.exists(args.key):
+        key = get_file_data(args.key)
+    else:
+        key =  ''.join([str(random.randint(0, 9)) for i in range(len(text))])
     
-      #  alp_path = sys.argv[2]
+    main(action, text, key, args.output, args.logs)
 
-   # alp = get_alphabet(alp_path)
-    #
-    main(action, text, key, args.output)
-
-    #write_data('decrypted.txt', a.decrypt(text))
+    print('Время выполнения:',time.time() - start_time, 'секунд')
